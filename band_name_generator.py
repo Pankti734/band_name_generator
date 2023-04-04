344fb658800cd0aea834705e7a7430703e744646
Pankti734 <126952854+Pankti734@users.noreply.github.com>
2023-04-05 00:32:50 +0530

    Create band_name_generator.py

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator.")
+street=input("What's the name of the city you grew up in?\n")
+pet=input("What's the name of your pet?")
+print("Your band name could be"+ street +" "+ pet)
